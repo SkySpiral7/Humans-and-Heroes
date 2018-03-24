ac368975b9004b02656c8152b01a6321d0e77f15
Ryan Worcester <rworcest@g.emporia.edu>
2018-03-23 19:29:53 -0500

    Xml to js: civilians, trained-combatants

---
+++ examples/characters/civilians/js/Reporter.js
@@ -0,0 +1,116 @@
+var json = {
+   "Hero": {
+      "name": "Reporter",
+      "transcendence": 0,
+      "image": "../images/Sirocco.jpg"
+   },
+   "Abilities": {
+      "Strength": 0,
+      "Agility": 0,
+      "Fighting": 0,
+      "Awareness": 2,
+      "Stamina": 0,
+      "Dexterity": 0,
+      "Intellect": 2,
+      "Presence": 1
+   },
+   "Powers": [],
+   "Equipment": [
+      {
+         "effect": "Feature",
+         "cost": 1,
+         "text": "Camera, computer, audio recorder",
+         "action": "None",
+         "range": "Personal",
+         "duration": "Permanent",
+         "Modifiers": [],
+         "rank": 3
+      },
+      {
+         "effect": "Feature",
+         "cost": 2,
+         "text": "smart-phone",
+         "action": "None",
+         "range": "Personal",
+         "duration": "Permanent",
+         "Modifiers": [],
+         "rank": 1
+      }
+   ],
+   "Advantages": [
+      {
+         "name": "Equipment",
+         "rank": 1
+      },
+      {"name": "Connected"}
+   ],
+   "Skills": [
+      {
+         "name": "Deception",
+         "subtype": "",
+         "rank": 4,
+         "ability": "Presence"
+      },
+      {
+         "name": "Knowledge",
+         "subtype": "Current Events",
+         "rank": 4,
+         "ability": "Intellect"
+      },
+      {
+         "name": "Knowledge",
+         "subtype": "Pop Culture",
+         "rank": 2,
+         "ability": "Intellect"
+      },
+      {
+         "name": "Expertise",
+         "subtype": "Streetwise",
+         "rank": 2,
+         "ability": "Intellect"
+      },
+      {
+         "name": "Expertise",
+         "subtype": "Journalism",
+         "rank": 4,
+         "ability": "Intellect"
+      },
+      {
+         "name": "Investigation",
+         "subtype": "",
+         "rank": 2,
+         "ability": "Intellect"
+      },
+      {
+         "name": "Perception",
+         "rank": 4,
+         "ability": "Awareness"
+      },
+      {
+         "name": "Persuasion",
+         "rank": 4,
+         "ability": "Presence"
+      },
+      {
+         "name": "Stealth",
+         "subtype": "",
+         "rank": 4,
+         "ability": "Agility"
+      },
+      {
+         "name": "Vehicles",
+         "subtype": "",
+         "rank": 2,
+         "ability": "Dexterity"
+      }
+   ],
+   "Defenses": {
+      "Dodge": 0,
+      "Fortitude": 0,
+      "Parry": 0,
+      "Will": 1
+   },
+   "ruleset": "3.11",
+   "version": 2,
+   "Information": ""
+};
---
+++ examples/characters/trained-combatants/js/Soldier.js
@@ -0,0 +1,114 @@
+var json = {
+   "Hero": {
+      "name": "Soldier",
+      "transcendence": 0,
+      "image": "../images/Sirocco.jpg"
+   },
+   "Abilities": {
+      "Strength": 1,
+      "Agility": 1,
+      "Fighting": 5,
+      "Awareness": 0,
+      "Stamina": 2,
+      "Dexterity": 1,
+      "Intellect": 0,
+      "Presence": 0
+   },
+   "Powers": [],
+   "Equipment": [
+      {
+         "effect": "Damage",
+         "text": "",
+         "action": "Standard",
+         "range": "Ranged",
+         "duration": "Instant",
+         "name": "Assault rifle",
+         "skill": "Rifles",
+         "Modifiers": [
+            {
+               "name": "Increased Range",
+               "applications": 1
+            },
+            {"name": "Multiattack"}
+         ],
+         "rank": 5
+      },
+      {
+         "effect": "Protection",
+         "text": "body armor",
+         "action": "None",
+         "range": "Personal",
+         "duration": "Permanent",
+         "Modifiers": [],
+         "rank": 3
+      },
+      {
+         "effect": "Feature",
+         "cost": 1,
+         "text": "17 points of other equipment",
+         "action": "None",
+         "range": "Personal",
+         "duration": "Permanent",
+         "Modifiers": [],
+         "rank": 17
+      }
+   ],
+   "Advantages": [
+      {
+         "name": "Equipment",
+         "rank": 7
+      }
+   ],
+   "Skills": [
+      {
+         "name": "Ranged Combat",
+         "subtype": "Rifles",
+         "rank": 4,
+         "ability": "Dexterity"
+      },
+      {
+         "name": "Athletics",
+         "subtype": "",
+         "rank": 4,
+         "ability": "Strength"
+      },
+      {
+         "name": "Expertise",
+         "subtype": "Soldier",
+         "rank": 4,
+         "ability": "Intellect"
+      },
+      {
+         "name": "Strategy",
+         "subtype": "Military",
+         "rank": 4,
+         "ability": "Intellect"
+      },
+      {
+         "name": "Intimidation",
+         "subtype": "",
+         "rank": 2,
+         "ability": "Presence"
+      },
+      {
+         "name": "Perception",
+         "rank": 2,
+         "ability": "Awareness"
+      },
+      {
+         "name": "Vehicles",
+         "subtype": "",
+         "rank": 2,
+         "ability": "Dexterity"
+      }
+   ],
+   "Defenses": {
+      "Dodge": 4,
+      "Fortitude": 3,
+      "Parry": 0,
+      "Will": 1
+   },
+   "ruleset": "3.11",
+   "version": 2,
+   "Information": ""
+};
